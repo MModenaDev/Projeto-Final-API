65dd36b0dc1e3a71f43d978026826beafc4ba8cc
Anedom <marco.modena@tutanota.com>
2019-10-24 15:40:50 -0300

    basic api/postman structure

---
+++ app.js
@@ -0,0 +1,92 @@
+require('dotenv').config();
+
+const bodyParser = require('body-parser');
+const cookieParser = require('cookie-parser');
+const express = require('express');
+const favicon = require('serve-favicon');
+const mongoose = require('mongoose');
+const logger = require('morgan');
+const path = require('path');
+const cors = require('cors');
+
+
+// WHEN INTRODUCING USERS DO THIS:
+// INSTALL THESE DEPENDENCIES: passport-local, passport, bcryptjs, express-session
+// AND UN-COMMENT OUT FOLLOWING LINES:
+
+// const session = require('express-session');
+// const passport = require('passport');
+// const MongoStore = require('connect-mongo')(session)
+
+// require('./configs/passport');
+
+// IF YOU STILL DIDN'T, GO TO 'configs/passport.js' AND UN-COMMENT OUT THE WHOLE FILE
+
+mongoose
+  .connect(process.env.MONGODB_URI, {
+    useNewUrlParser: true
+  })
+  .then(x => {
+    console.log(`Connected to Mongo! Database name: "${x.connections[0].name}"`)
+  })
+  .catch(err => {
+    console.error('Error connecting to mongo', err)
+  });
+
+const app_name = require('./package.json').name;
+const debug = require('debug')(`${app_name}:${path.basename(__filename).split('.')[0]}`);
+
+const app = express();
+
+// Middleware Setup
+app.use(logger('dev'));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({
+  extended: false
+}));
+app.use(cookieParser());
+
+// Express View engine setup
+
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'hbs');
+app.use(express.static(path.join(__dirname, 'public')));
+
+// ADD SESSION SETTINGS HERE:
+
+// app.use(session({
+//   secret: "fakebook",
+//   resave: true,
+//   saveUninitialized: true,
+//   store: new MongoStore({
+//     mongooseConnection: mongoose.connection,
+//     ttl: 24 * 60 * 60
+//   })
+// }));
+
+// USE passport.initialize() and passport.session() HERE:
+// app.use(passport.initialize());
+// app.use(passport.session());
+
+// default value for title local
+app.locals.title = 'Final Project';
+
+
+// ADD CORS SETTINGS HERE TO ALLOW CROSS-ORIGIN INTERACTION:
+
+app.use(cors({
+  credentials: true,
+  origin: ['http://localhost:3000'] // <== this will be the URL of our React app (it will be running on port 3000)
+}));
+
+// ROUTES MIDDLEWARE STARTS HERE:
+
+// app.use('/', require('./routes/auth'));
+
+
+app.use((req, res, next) => {
+  res.sendFile(__dirname + "/public/index.html");
+});
+
+
+module.exports = app;
